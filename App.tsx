74c358aa2f5084c2087c387ffb5f5118a6f5ba12
Nagisa Kaworu <fujimori_kenta@cyberagent.co.jp>
2024-10-02 22:53:40 +0900

    feat:firebaseとの接続とデータの取得

--- App.tsx
+++ App.tsx
@@ -1,13 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react'
+import type { Shop } from './src/types/shop'
+import { getShops } from './src/lib/firebase'
 
 export default function App() {
-  return (
+
+  const [shops, setShops] = useState<Shop[]>([])
+
+  useEffect(() => {
+    getFirebaseItems()
+  }, [])
+
+  const getFirebaseItems = async() => {
+    const shops = await getShops()
+    setShops(shops)
+  }
+
+  return(
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {shops.map((shop, index) => {
+        return (
+          <Text key={index}>
+            {shop.name} {shop.place}
+          </Text>
+        )
+      })}
     </View>
-  );
+  )
+
 }
 
 const styles = StyleSheet.create({
---
+++ src/lib/firebase.ts
@@ -0,0 +1,32 @@
+
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { getFirestore, collection, getDocs, orderBy, query } from 'firebase/firestore'
+import type { Shop } from '../types/shop'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_FB_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FB_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FB_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FB_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FB_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FB_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// // const auth = initializeAuth(app, {
+// //   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+// // })
+const db = getFirestore(app)
+
+export const getShops = async() => {
+  const querySnapshot = await getDocs(collection(db, 'shops'))
+  const shops = querySnapshot.docs.map((doc) => doc.data() as Shop)
+  return shops
+}
